dceb9bc00e03bfb33b3813463b8ca68a817a153e
DanCarCo <120357371+DanCarCo@users.noreply.github.com>
2026-03-28 10:34:44 -0500

    Cliente

---
+++ Clases/Cliente.java
@@ -0,0 +1,8 @@
+public class Cliente extends Usuario {
+
+    public Cliente(String nombre, String email, String telefono) {
+       this.nombre = nombre;
+       this.email = email;
+        this.telefono = telefono;
+    }
+}
